9e2bce1acbdef20bd4a61a8f19c00039ec8ea5e8
Uve Rogre <uverogre@gmail.com>
2020-07-09 17:52:46 +0200

    Add note vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,7 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('create-note', require('./components/CreateNote.vue').default);
+Vue.component('note', require('./components/Note.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
