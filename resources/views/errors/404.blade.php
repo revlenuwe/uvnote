2cd86c5e1f7e856cb266ff3f0be8748ea4ea333b
Uve Rogre <uverogre@gmail.com>
2020-07-11 12:11:03 +0200

    Add custom 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.guest')
+
+@section('content')
+    <div class="tab-content" id="app">
+        <section class="space-ptb">
+            <div class="container">
+                <div class="col-lg-12 col-md-6 mt-5 text-center">
+                    <div id="notfound">
+                        <div class="notfound">
+
+                            <h2>404 - Page not found</h2>
+                            <p>The page you are looking for might have been removed</p>
+                            <a class="btn btn-primary" href="/">Go To Homepage</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+@endsection
